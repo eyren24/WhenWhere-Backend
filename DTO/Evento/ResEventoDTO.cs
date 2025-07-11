be50a024b18ea8b51ea32d88faf7349ca34f32d6
eyren24 <idk.who.im24@gmail.com>
2025-07-11 11:56:57 +0200

    aggiunta evento e nota DTO

---
+++ DTO/Evento/ResEventoDTO.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DTO.Evento;
+
+public class ResEventoDTO
+{
+    
+    [Key]
+    [Required]
+    public int id { get; set; }
+
+    [Required]
+    public int agendaId { get; set; }
+
+    [Column(TypeName = "datetime")]
+    [Required]
+    public DateTime dataInizio { get; set; }
+
+    [Column(TypeName = "datetime")]
+    [Required]
+    public DateTime? dataFine { get; set; } = null;
+
+    [Column(TypeName = "text")] [Required] public string? descrizione { get; set; } = null;
+
+    [Column(TypeName = "decimal(1, 1)")]
+    [Required]
+    public decimal? rating { get; set; } = null;
+
+    [StringLength(50)]
+    [Required]
+    public string luogo { get; set; } = null!;
+
+    [StringLength(50)]
+    [Required]
+    public string stato { get; set; } = null!;
+
+    [Required]
+    public bool notifica { get; set; }
+
+    [Column(TypeName = "datetime")]
+    [Required]
+    public DateTime dataCreazione { get; set; }
+
+    [StringLength(50)]
+    [Required]
+    public string titolo { get; set; } = null!;
+
+    [Required]
+    public int tagId { get; set; }
+}
---
+++ DTO/Nota/ReqNotaDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DTO.Nota;
+
+public class ReqNotaDTO
+{
+    [Required] public int agendaId { get; set; }
+    [StringLength(50)] public string titolo { get; set; } = null!;
+    [Column(TypeName = "text")] [Required] public string? descrizione { get; set; }
+    [Column(TypeName = "datetime")]
+    [Required]
+    public DateTime dataCreazione { get; set; }
+    [StringLength(50)] public string tema { get; set; } = null!;
+    [Required] public int tagId { get; set; }
+}
