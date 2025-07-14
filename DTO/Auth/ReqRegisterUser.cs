0da5c574d785c9a0403d03f9d1f371c1acbc9e38
eyren24 <idk.who.im24@gmail.com>
2025-07-14 11:04:05 +0200

    ciao

---
+++ DTO/Auth/ReqRegisterUser.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DTO.Auth;
+
+public class ReqRegisterUser
+{
+    [StringLength(50)]
+    [Required]
+    public string nome { get; set; } = null!;
+
+    [StringLength(50)]
+    [Required]
+    public string cognome { get; set; } = null!;
+
+    [StringLength(50)]
+    [Required]
+    public string email { get; set; } = null!;
+
+    [Required]
+    public string password { get; set; } = null!;
+    [Required]
+    public string confermaPassword { get; set; } = null!;
+
+    [Required]
+    public DateOnly dataNascita { get; set; }
+
+    [StringLength(50)]
+    [Required]
+    public string genere { get; set; } = null!;
+}
